a68204412c035b72161d0725a2bd9de3e4dc397b
CryptoManiac <balthazar@yandex.ru>
2015-08-16 23:04:14 +0300

    CKey constructors

--- Novacoin/CKey.cs
+++ Novacoin/CKey.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 
 namespace Novacoin
 {
+    /// <summary>
+    /// Representation of ECDSA private key
+    /// </summary>
     public class CKey
     {
+        /// <summary>
+        /// Private key bytes
+        /// </summary>
+        private List<byte> privKeyBytes;
+
+        /// <summary>
+        /// Initialize new instance of CKey as copy of another instance.
+        /// </summary>
+        /// <param name="key">New CKey instance.</param>
+        public CKey(CKey key)
+        {
+            privKeyBytes = key.privKeyBytes;
+        }
+
+        /// <summary>
+        /// Initialize new instance of CKey using supplied byte sequence.
+        /// </summary>
+        /// <param name="bytes">New CKey instance.</param>
+        public CKey(IEnumerable<byte> bytes)
+        {
+            privKeyBytes = new List<byte>(bytes);
+        }
+
+        /// <summary>
+        /// Calculate public key for this private key.
+        /// </summary>
+        /// <returns>New CPubKey instance.</returns>
         public CPubKey GetPubKey()
         {
             // stub
 
-            return new CPubKey();
+            return new CPubKey((CPubKey)null);
         }
     }
 }
