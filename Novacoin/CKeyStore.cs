e35f7ea545839a56961ab45e57a81a857aeaefc2
CryptoManiac <balthazar@yandex.ru>
2015-08-25 02:18:24 +0300

    Add CKeyStore stub

---
+++ Novacoin/CKeyStore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novacoin
+{
+    public class CKeyStore
+    {
+        public CKeyStore(string strDatabasePath="Wallet.db")
+        {
+        }
+
+        ~CKeyStore()
+        {
+        }
+
+        public bool AddKey(CKeyPair keyPair)
+        {
+            // TODO
+
+            return true;
+        }
+
+        public bool HaveKey(CKeyID keyID)
+        {
+            // TODO
+
+            return true;
+        }
+
+        public bool GetKey(CKeyID keyID, out CKeyPair keyPair)
+        {
+            keyPair = new CKeyPair();
+
+            return false;
+        }
+
+        public bool AddScript(CScript script)
+        {
+            // TODO
+
+            return true;
+        }
+
+        public bool HaveScript(CScriptID scriptID)
+        {
+            // TODO
+
+            return true;
+        }
+
+        public bool GetScript(CScriptID scriptID, out CScript script)
+        {
+            // TODO
+
+            script = new CScript();
+
+            return true;
+        }
+
+
+    }
+}
